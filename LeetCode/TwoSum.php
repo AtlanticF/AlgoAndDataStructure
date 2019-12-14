4fe4dd94263a574e71ebce0f3b852ac6449f4d5d
matt <meifeng.song@ankerjiedian.com>
2019-12-14 12:12:24 +0800

    修改部分注释；leetcode 解题: 两数之和

--- LeetCode/TwoSum.php
+++ LeetCode/TwoSum.php
@@ -1,12 +1,54 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: matt
- * Date: 2019/12/9
- * Time: 下午10:06
+ * 两数之和 leetcode: 1
  */
 
 class TwoSum
 {
+    /**
+     * 两数之和，利用 hashMap
+     *
+     * 时间复杂度 O(n) 空间复杂度 O(n)
+     *
+     * @param array $arr
+     * @param int $target
+     * @return array
+     */
+    public function towSum(array $arr, int $target): array
+    {
+        $len = count($arr) - 1;
+        $map = array();
+        for ($i = 0; $i < $len; $i++) {
+            $need = $target - $arr[$i];
+            if (isset($map[$need])) {
+                return [$map[$need], $i];
+            }
+            $map[$arr[$i]] = $i;
+        }
 
+        return [-1, -1];
+    }
+
+    /**
+     * 两数之和，暴力法
+     *
+     * 时间复杂度 O(n^2) 空间复杂度 O(1)
+     *
+     * @param array $arr
+     * @param int $target
+     * @return array
+     */
+    public function twoSum2(array $arr, int $target): array
+    {
+        $len = count($arr);
+        for ($i = 0; $i < $len; $i++) {
+            for ($j = 1; $j < $len; $j++) {
+                if (($arr[$i] + $arr[$j]) == $target) {
+                    return [$i, $j];
+                }
+            }
+        }
+
+        return [-1, -1];
+    }
 }
